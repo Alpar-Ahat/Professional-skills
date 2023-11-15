5255fdf9cb02ef3c4b771ff0be5ffa0333dac9aa
Alpar-Ahat <150819354+Alpar-Ahat@users.noreply.github.com>
2023-11-15 19:07:23 +0000

    Add files via upload

---
+++ googlettest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Durham/MISCADA/Professional skill/GitHub/googletest/googletest
+# Build directory: E:/Durham/MISCADA/Professional skill/GitHub/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
